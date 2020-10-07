bc6a021749f5f5c03c80b472ab8f8e06abcd2274
jessicataylor1 <68263365+jessicataylor1@users.noreply.github.com>
2020-10-07 20:52:02 +1300

    commenting

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+/* Mobile burger menu */
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
